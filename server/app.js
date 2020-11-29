48987e39cead53dac6edc0ef67e4cc3f0a0c84cc
Anthony <t9haan01@students.oamk.fi>
2020-11-29 22:09:24 +0200

    Read test

--- server/app.js
+++ server/app.js
@@ -4,16 +4,17 @@ class MessageApp {
   }
 
   post(content) {
-      let item = {
-          id: this.messages.length,
-          content: content,
-          date: new Date()
-      }
-      this.messages.push(item)
-      return this.messages
+    let item = {
+      id: this.messages.length,
+      content: content,
+      date: new Date(),
+    };
+    this.messages.push(item);
+    return this.messages;
   }
-
-  
+  get(id) {
+    return this.messages[id];
+  }
 }
 
 export default MessageApp;
